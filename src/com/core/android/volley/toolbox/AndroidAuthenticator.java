460f36013170c400342712df15a9a47f84c5bc72
yangboqing <yangboqing1234@126.com>
2015-09-01 15:05:45 +0800

    dd

--- src/com/core/android/volley/toolbox/AndroidAuthenticator.java
+++ src/com/core/android/volley/toolbox/AndroidAuthenticator.java
@@ -73,8 +73,6 @@ public class AndroidAuthenticator implements Authenticator {
         return mAccount;
     }
 
-    // TODO: Figure out what to do about notifyAuthFailure
-    @SuppressWarnings("deprecation")
     @Override
     public String getAuthToken() throws AuthFailureError {
         AccountManagerFuture<Bundle> future = mAccountManager.getAuthToken(mAccount,
